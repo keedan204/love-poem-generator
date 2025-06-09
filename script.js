d7f03472496a9b3087095f18ee81da96307e001b
keedan204 <danikaymonique@gmail.com>
2025-06-08 19:55:23 -0500

    Added basic form handling and added typewriter plugin

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert("Hello World");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "This is where the poem will go",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+  poemElement.innerHTML = "";
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
